691928720a68f95d6e116e43bb6aa5808f095140
Ishan Bhavsar <bhavsarishan2@gmail.com>
2021-03-25 21:46:59 +0530

    ok tested

---
+++ HelloWorld.js
@@ -0,0 +1,25 @@
+alert("Hello World.")
+
+var a = "The World is a nice place.";
+
+console.log(a);
+console.log("shit.")
+
+var num = 24;
+console.log(num)
+
+
+var name = prompt("What is your name?");
+var age = prompt("What is your age?");
+var bane = prompt("Are you a boon to the world?")
+
+document.getElementById('fl1').innerHTML = "My name is Ishan";
+
+document.getElementById('fl2').innerHTML = name;
+
+document.getElementById('fl4').innerHTML = "Your name is "+name+" and your age is "+age+" years.";
+
+document.getElementById('fl5').innerHTML = bane;
+
+
+
