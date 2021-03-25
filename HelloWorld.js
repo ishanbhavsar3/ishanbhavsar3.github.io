7fbdc69b9b220b47094135903dd47cfb2bbcfb70
Ishan Bhavsar <bhavsarishan2@gmail.com>
2021-03-25 21:54:34 +0530

    Update final both

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,14 +1,1 @@
-alert("Welcome to Ishan's website! ")
-
-
-var name = prompt("What is your name?");
-var age = prompt("What is your age?");
-
-
-document.getElementById('fl1').innerHTML = "Owner's name is Ishan";
-
-document.getElementById('fl4').innerHTML = "Your name is "+name+" and your age is "+age+" years.";
-
-
-
 
